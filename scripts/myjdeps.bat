307871a07f2adc985c6b11cfc4586c018c31c5e4
nmby <nmby@hotchpotch.xyz>
2020-07-17 21:49:58 +0900

    Javaのバージョン更新（13->14）

--- scripts/myjdeps.bat
+++ scripts/myjdeps.bat
@@ -2,10 +2,10 @@
 @echo OFF
 chcp 65001
 
-set JDEPS_CMD=c:\pleiades_201909\java\13\bin\jdeps
+set JDEPS_CMD=c:\pleiades_202006\java\14\bin\jdeps
 set VERSION=0.7.2
 
-set MODULE_PATH_JRE=C:\pleiades_201909\java\13\jmods
+set MODULE_PATH_JRE=C:\pleiades_202006\java\14\jmods
 set MODULE_PATH_JAVAFX=C:\Users\ya_na\OneDrive\UserLibs\javafx-sdk-13\lib
 set PATH_POI=C:\Users\ya_na\OneDrive\UserLibs\poi-4.1.2
 set MODULE_PATH_POI=%PATH_POI%\poi-4.1.2.jar;%PATH_POI%\poi-ooxml-4.1.2.jar;%PATH_POI%\poi-ooxml-schemas-4.1.2.jar;%PATH_POI%\lib\commons-codec-1.13.jar;%PATH_POI%\lib\commons-collections4-4.4.jar;%PATH_POI%\lib\commons-compress-1.19.jar;%PATH_POI%\lib\commons-math3-3.6.1.jar;%PATH_POI%\lib\SparseBitSet-1.2.jar;%PATH_POI%\ooxml-lib\xmlbeans-3.1.0.jar
