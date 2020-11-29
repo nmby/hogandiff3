a5493d059f246b41693b3e40662185bf25d3d945
nmby <nmby@hotchpotch.xyz>
2020-11-29 09:05:21 +0900

    #44 最大ヒープサイズの変更（1024MB→2048MB）

--- resources/方眼Diff.app/Contents/MacOS/Launcher.sh
+++ resources/方眼Diff.app/Contents/MacOS/Launcher.sh
@@ -2,4 +2,4 @@
 
 APP_ROOT=$(cd $(dirname $0); cd ..; pwd)
 
-$APP_ROOT/PlugIns/jre-min/bin/java -Xms256m -Xmx1024m -jar $APP_ROOT/Java/xyz.hotchpotch.hogandiff.jar $@ &
+$APP_ROOT/PlugIns/jre-min/bin/java -Xms256m -Xmx2048m -jar $APP_ROOT/Java/xyz.hotchpotch.hogandiff.jar $@ &
