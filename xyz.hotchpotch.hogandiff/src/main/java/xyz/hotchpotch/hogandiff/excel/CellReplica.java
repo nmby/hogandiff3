bca938da6ea0c113d9e09796db7c78ae40c9649b
nmby <nmby@hotchpotch.xyz>
2021-05-08 13:58:04 +0900

    リファクタリング：convert CellReplica into Record

--- xyz.hotchpotch.hogandiff/src/main/java/xyz/hotchpotch/hogandiff/excel/CellReplica.java
+++ xyz.hotchpotch.hogandiff/src/main/java/xyz/hotchpotch/hogandiff/excel/CellReplica.java
@@ -11,7 +11,11 @@ import xyz.hotchpotch.hogandiff.util.Pair;
  *
  * @author nmby
  */
-public class CellReplica {
+public record CellReplica(
+        int row,
+        int column,
+        String content,
+        String comment) {
     
     // [static members] ********************************************************
     
@@ -163,38 +167,11 @@ public class CellReplica {
     
     // [instance members] ******************************************************
     
-    private final int row;
-    private final int column;
-    private String content;
-    private String comment;
-    
-    private CellReplica(int row, int column, String content, String comment) {
-        assert 0 <= row;
-        assert 0 <= column;
-        assert content != null;
-        
-        this.row = row;
-        this.column = column;
-        this.content = content;
-        this.comment = comment;
-    }
-    
-    /**
-     * 行インデックス（0開始）を返します。<br>
-     * 
-     * @return 行インデックス（0開始）
-     */
-    public int row() {
-        return row;
-    }
-    
-    /**
-     * 列インデックス（0開始）を返します。<br>
-     * 
-     * @return 列インデックス（0開始）
-     */
-    public int column() {
-        return column;
+    public CellReplica {
+        Objects.requireNonNull(content, "content");
+        if (row < 0 || column < 0) {
+            throw new IllegalArgumentException("row==%d, column==%d".formatted(row, column));
+        }
     }
     
     /**
@@ -204,51 +181,6 @@ public class CellReplica {
      */
     public String address() {
         return idxToAddress(row(), column());
-    }
-    
-    /**
-     * セル内容を返します。<br>
-     * セル内容が無い場合は {@code ""} を返します。
-     * このメソッドが {@code null} を返すことはありません。<br>
-     * 
-     * @return セル内容
-     */
-    public String content() {
-        return content;
-    }
-    
-    /**
-     * セル内容を設定します。<br>
-     * 
-     * @param content セル内容
-     * @throws NullPointerException {@code content} が {@code null} の場合
-     */
-    @Deprecated
-    public void setContent(String content) {
-        Objects.requireNonNull(content, "content");
-        
-        this.content = content;
-    }
-    
-    /**
-     * セルコメントを返します。<br>
-     * セルコメントが無い場合は {@code null} を、
-     * セルコメントの内容が空の場合は {@code ""} を返します。<br>
-     * 
-     * @return セルコメント
-     */
-    public String comment() {
-        return comment;
-    }
-    
-    /**
-     * セルコメントを設定します。<br>
-     * 
-     * @param comment セルコメント
-     */
-    @Deprecated
-    public void setComment(String comment) {
-        this.comment = comment;
     }
     
     /**
@@ -281,27 +213,6 @@ public class CellReplica {
                 : comment != null && cell.comment != null
                         ? comment.compareTo(cell.comment)
                         : 0;
-    }
-    
-    /**
-     * {@code o} も {@link CellReplica} であり、
-     * {@link CellReplica#row()}, {@link CellReplica#column()} の値がそれぞれ等しく、
-     * {@link CellReplica#content()} が同値と判定される場合に
-     * {@code true} を返します。<br>
-     */
-    @Override
-    public boolean equals(Object o) {
-        if (o instanceof CellReplica other) {
-            return row == other.row()
-                    && column == other.column()
-                    && attrEquals(other);
-        }
-        return false;
-    }
-    
-    @Override
-    public int hashCode() {
-        return Objects.hash(row, column, content, comment);
     }
     
     @Override
