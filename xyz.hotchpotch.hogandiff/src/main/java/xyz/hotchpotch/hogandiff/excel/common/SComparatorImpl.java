7153651ba43d9191186b21ba5a2c685321eb5c4a
nmby <nmby@hotchpotch.xyz>
2022-11-30 23:54:01 +0900

    SComparatorImplの汎用部分をSComparatorBaseに分離

--- xyz.hotchpotch.hogandiff/src/main/java/xyz/hotchpotch/hogandiff/excel/common/SComparatorImpl.java
+++ xyz.hotchpotch.hogandiff/src/main/java/xyz/hotchpotch/hogandiff/excel/common/SComparatorImpl.java
@@ -4,57 +4,24 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import xyz.hotchpotch.hogandiff.core.Matcher;
 import xyz.hotchpotch.hogandiff.excel.CellData;
-import xyz.hotchpotch.hogandiff.excel.CellsUtil;
 import xyz.hotchpotch.hogandiff.excel.SComparator;
-import xyz.hotchpotch.hogandiff.excel.SResult;
 import xyz.hotchpotch.hogandiff.util.IntPair;
-import xyz.hotchpotch.hogandiff.util.Pair;
 
 /**
  * {@link SComparator} の標準的な実装です。<br>
  *
  * @author nmby
  */
-public class SComparatorImpl implements SComparator {
+public class SComparatorImpl extends SComparatorBase {
     
     // [static members] ********************************************************
-    
-    private static final int[] EMPTY_INT_ARRAY = new int[] {};
-    
-    /**
-     * 行同士または列同士の対応関係を決定するマッパーを表します。<br>
-     * これは、{@link #makePairs(Set, Set)} を関数メソッドに持つ関数型インタフェースです。<br>
-     *
-     * @author nmby
-     */
-    @FunctionalInterface
-    private static interface Mapper {
-        
-        // [static members] ----------------------------------------------------
-        
-        // [instance members] --------------------------------------------------
-        
-        /**
-         * 行同士または列同士の対応関係を決定し、
-         * インデックスのペアのリストとして返します、<br>
-         * 
-         * @param cells1 セルセット1
-         * @param cells2 セルセット2
-         * @return 行同士または列同士の対応関係
-         */
-        List<IntPair> makePairs(
-                Set<CellData> cells1,
-                Set<CellData> cells2);
-    }
     
     /**
      *  縦方向の余剰／欠損を考慮しない場合のマッパーを返します。<br>
@@ -256,131 +223,37 @@ public class SComparatorImpl implements SComparator {
     
     // [instance members] ******************************************************
     
-    private final boolean considerRowGaps;
-    private final boolean considerColumnGaps;
-    private final boolean saveMemory;
-    private final Mapper rowsMapper;
-    private final Mapper columnsMapper;
-    
     private SComparatorImpl(
             boolean considerRowGaps,
             boolean considerColumnGaps,
             boolean saveMemory) {
         
-        this.considerRowGaps = considerRowGaps;
-        this.considerColumnGaps = considerColumnGaps;
-        this.saveMemory = saveMemory;
-        
-        if (considerRowGaps && considerColumnGaps) {
-            rowsMapper = mapper(CellData::row, CellData::dataCompareTo);
-            columnsMapper = mapper(CellData::column, CellData::dataCompareTo);
-        } else if (considerRowGaps) {
-            rowsMapper = mapper(CellData::row, Comparator.comparingInt(CellData::column));
-            columnsMapper = mapper(CellData::column);
-        } else if (considerColumnGaps) {
-            rowsMapper = mapper(CellData::row);
-            columnsMapper = mapper(CellData::column, Comparator.comparingInt(CellData::row));
-        } else {
-            rowsMapper = mapper(CellData::row);
-            columnsMapper = mapper(CellData::column);
-        }
+        super(considerRowGaps, considerColumnGaps, saveMemory);
     }
     
-    /**
-     * {@inheritDoc}
-     * 
-     * @throws NullPointerException
-     *              {@code cells1}, {@code cells2} のいずれかが {@code null} の場合
-     * @throws IllegalArgumentException
-     *              {@code cells1}, {@code cells2} が同一インスタンスの場合
-     */
     @Override
-    public SResult compare(
-            Set<CellData> cells1,
-            Set<CellData> cells2) {
-        
-        Objects.requireNonNull(cells1, "cells1");
-        Objects.requireNonNull(cells2, "cells2");
-        
-        if (cells1 == cells2) {
-            if (cells1.isEmpty()) {
-                return new SResult(
-                        considerRowGaps,
-                        considerColumnGaps,
-                        Pair.of(EMPTY_INT_ARRAY, EMPTY_INT_ARRAY),
-                        Pair.of(EMPTY_INT_ARRAY, EMPTY_INT_ARRAY),
-                        List.of());
-            } else {
-                throw new IllegalArgumentException("cells1 == cells2");
-            }
+    protected Mapper rowsMapper() {
+        if (considerRowGaps && considerColumnGaps) {
+            return mapper(CellData::row, CellData::dataCompareTo);
+        } else if (considerRowGaps) {
+            return mapper(CellData::row, Comparator.comparingInt(CellData::column));
+        } else if (considerColumnGaps) {
+            return mapper(CellData::row);
+        } else {
+            return mapper(CellData::row);
         }
-        
-        List<IntPair> rowPairs = rowsMapper.makePairs(cells1, cells2);
-        List<IntPair> columnPairs = columnsMapper.makePairs(cells1, cells2);
-        
-        // 余剰行の収集
-        int[] redundantRows1 = rowPairs.stream()
-                .filter(IntPair::isOnlyA).mapToInt(IntPair::a).toArray();
-        int[] redundantRows2 = rowPairs.stream()
-                .filter(IntPair::isOnlyB).mapToInt(IntPair::b).toArray();
-        
-        // 余剰列の収集
-        int[] redundantColumns1 = columnPairs.stream()
-                .filter(IntPair::isOnlyA).mapToInt(IntPair::a).toArray();
-        int[] redundantColumns2 = columnPairs.stream()
-                .filter(IntPair::isOnlyB).mapToInt(IntPair::b).toArray();
-        
-        // 差分セルの収集
-        List<Pair<CellData>> diffCells = extractDiffs(
-                cells1, cells2, rowPairs, columnPairs);
-        
-        return new SResult(
-                considerRowGaps,
-                considerColumnGaps,
-                Pair.of(redundantRows1, redundantRows2),
-                Pair.of(redundantColumns1, redundantColumns2),
-                diffCells);
     }
     
-    private List<Pair<CellData>> extractDiffs(
-            Set<CellData> cells1,
-            Set<CellData> cells2,
-            List<IntPair> rowPairs,
-            List<IntPair> columnPairs) {
-        
-        assert cells1 != null;
-        assert cells2 != null;
-        assert cells1 != cells2;
-        assert rowPairs != null;
-        assert columnPairs != null;
-        
-        // TODO: 要処理内容見直し
-        
-        Map<String, CellData> map1 = cells1.stream()
-                .collect(Collectors.toMap(CellData::address, Function.identity()));
-        Map<String, CellData> map2 = cells2.stream()
-                .collect(Collectors.toMap(CellData::address, Function.identity()));
-        
-        List<IntPair> columnPairsFiltered = columnPairs.stream().filter(IntPair::isPaired).toList();
-        
-        return rowPairs.parallelStream().filter(IntPair::isPaired).flatMap(rp -> {
-            int row1 = rp.a();
-            int row2 = rp.b();
-            
-            return columnPairsFiltered.stream().map(cp -> {
-                int column1 = cp.a();
-                int column2 = cp.b();
-                String address1 = CellsUtil.idxToAddress(row1, column1);
-                String address2 = CellsUtil.idxToAddress(row2, column2);
-                CellData cell1 = map1.get(address1);
-                CellData cell2 = map2.get(address2);
-                
-                return (cell1 == null ? cell2 == null : cell1.dataEquals(cell2))
-                        ? null
-                        : Pair.<CellData> of(
-                                cell1 != null ? cell1 : CellData.empty(row1, column1, saveMemory),
-                                cell2 != null ? cell2 : CellData.empty(row2, column2, saveMemory));
-            }).filter(Objects::nonNull);
-        }).toList();
+    @Override
+    protected Mapper columnsMapper() {
+        if (considerRowGaps && considerColumnGaps) {
+            return mapper(CellData::column, CellData::dataCompareTo);
+        } else if (considerRowGaps) {
+            return mapper(CellData::column);
+        } else if (considerColumnGaps) {
+            return mapper(CellData::column, Comparator.comparingInt(CellData::row));
+        } else {
+            return mapper(CellData::column);
+        }
     }
 }
