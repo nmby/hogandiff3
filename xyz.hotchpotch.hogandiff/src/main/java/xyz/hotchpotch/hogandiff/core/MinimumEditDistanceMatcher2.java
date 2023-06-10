23231f4a7b8d1a3ec7c74bb7daa4856eb0c983d7
nmby <nmby@hotchpotch.xyz>
2023-06-10 20:03:49 +0900

    ソース微修正：コスト計算処理のパラレル化

--- xyz.hotchpotch.hogandiff/src/main/java/xyz/hotchpotch/hogandiff/core/MinimumEditDistanceMatcher2.java
+++ xyz.hotchpotch.hogandiff/src/main/java/xyz/hotchpotch/hogandiff/core/MinimumEditDistanceMatcher2.java
@@ -129,8 +129,8 @@ import xyz.hotchpotch.hogandiff.util.IntPair;
         assert listA != listB;
         
         // 1. リストA, リストBの要素の余剰コストを計算する。
-        int[] gapCostsA = listA.stream().mapToInt(gapEvaluator::applyAsInt).toArray();
-        int[] gapCostsB = listB.stream().mapToInt(gapEvaluator::applyAsInt).toArray();
+        int[] gapCostsA = listA.parallelStream().mapToInt(gapEvaluator::applyAsInt).toArray();
+        int[] gapCostsB = listB.parallelStream().mapToInt(gapEvaluator::applyAsInt).toArray();
         
         // 2. エディットグラフ上の各点の最小到達コストと最適遷移方向を計算する。
         //    比較対象リストが長くなるほど、すなわちエディットグラフ（探索平面）が広くなるほど
